310338dc5ec97425495163b6716974057133bb68
Ashley Cromack <Cromazone@live.co.uk>
2016-04-10 20:41:13 +0100

    End Game Stuff
    Now tries to give a more realistic end-game response, with typing time
    and added notes from the 'Tutor'.
    
    Currently painfully ugly, and draw-state needs changing in line, will
    change this into using seperate functions. Apologies for the horrible
    layout, in progress of streamlining

--- boardconfig.js
+++ boardconfig.js
@@ -126,13 +126,45 @@ var makeRandomMove = function() {
         // checkmate?
         if (game.in_checkmate() === true) {
             status = 'Game over, ' + moveColor + ' is in checkmate.';
-			document.getElementById("response").innerHTML = status;
+			if (playerTurn === true)
+			{
+				status = status + ' You\'ve heeded my advice well!';
+				
+				var typeTime = status.length * 60;
+				document.getElementById("typing").innerHTML = "Typing...";
+				//Time for typing
+				setTimeout(function() {
+					document.getElementById("response").innerHTML = status;
+					var d = new Date();
+					document.getElementById("typing").innerHTML = "Last message recieved at " + d.toLocaleTimeString();
+				}, typeTime);
+				
+				//document.getElementById("response").innerHTML = status;
+			}
+			else
+			{
+				status = status + ' Maybe follow my advice a bit more, huh?';
+				
+				var typeTime = status.length * 60;
+				document.getElementById("typing").innerHTML = "Typing...";
+				//Time for typing
+				setTimeout(function() {
+					document.getElementById("response").innerHTML = status;
+					var d = new Date();
+					document.getElementById("typing").innerHTML = "Last message recieved at " + d.toLocaleTimeString();
+				}, typeTime);
+				
+				
+				
+				//document.getElementById("response").innerHTML = status;
+			}
+			
         }
 
         // draw?
         else if (game.in_draw() === true) {
             status = 'Game over, drawn position';
-			document.getElementById("response").innerHTML = status;
+			document.getElementById("response").innerHTML = 'Your game has ended in a draw';
         }
 
         // game still on
